61444ab78c20871b864b2aa6acd7280ea42ecccf
Egor <steam.egor585@yandex.ru>
2025-07-01 18:12:24 +0300

    Обновил папку venv и добавил файл playwright_registration.py

--- main.py
+++
@@ -1,5 +0,0 @@
-def print_hi_owner_course(name):
-    print(f'Hi {name}!')
-
-if __name__ == '__main__':
-    print_hi_owner_course('Nikita Filonov')
---
+++ playwright_registration.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as playwright: 
+   chromium = playwright.chromium.launch(headless=False)
+   page = chromium.new_page()
+   page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+   registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+   expect(registration_email_input).to_be_visible()
+   registration_email_input.fill("user.name@gmail.com")
+
+   registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+   expect(registration_username_input).to_be_visible()
+   registration_username_input.fill("username")
+
+   registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+   expect(registration_password_input).to_be_visible()
+   registration_password_input.fill("password")
+
+   registration_button = page.get_by_test_id("registration-page-registration-button")
+   expect(registration_button).to_be_visible()
+   registration_button.click()
+
+   h6_dashboard_title = page.get_by_test_id("dashboard-toolbar-title-text")
+   expect(h6_dashboard_title).to_contain_text("Dashboard")
+   
+
+
+
