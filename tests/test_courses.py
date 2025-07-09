8e200bc56a6114b662090751a69d5c9a7df039b7
Egor <steam.egor585@yandex.ru>
2025-07-09 23:37:36 +0300

    Маркировки Pytest. Практика работы с pytest маркировкам.

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,12 +1,14 @@
+import pytest
 from playwright.sync_api import sync_playwright, expect 
 
-def test_empty_courses_list():
 
+@pytest.mark.courses
+@pytest.mark.regression
+def test_empty_courses_list():
     with sync_playwright() as playwright:
         browser = playwright.firefox.launch()
         context = browser.new_context()
         page = context.new_page()
-
         page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
         registration_button = page.get_by_test_id("registration-page-registration-button")
@@ -26,24 +28,22 @@ def test_empty_courses_list():
 
         expect(registration_button).not_to_be_disabled()
         registration_button.click()
-
         context.storage_state(path="browser-state.json")
 
     with sync_playwright() as playwright:
-            browser = playwright.firefox.launch()
-            context = browser.new_context(storage_state='browser-state.json')
-            page = context.new_page()
+        browser = playwright.firefox.launch()
+        context = browser.new_context(storage_state='browser-state.json')
+        page = context.new_page()
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-            page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+        courses_title_toolbar = page.get_by_test_id("courses-list-toolbar-title-text")
+        expect(courses_title_toolbar).to_contain_text("Courses")
 
-            courses_title_toolbar = page.get_by_test_id("courses-list-toolbar-title-text")
-            expect(courses_title_toolbar).to_contain_text("Courses")
+        courses_icon = page.get_by_test_id("courses-list-empty-view-icon")
+        expect(courses_icon).to_be_visible()
 
-            coureses_icon = page.get_by_test_id("courses-list-empty-view-icon")
-            expect(coureses_icon).to_be_visible()
+        courses_title = page.get_by_test_id("courses-list-empty-view-title-text")
+        expect(courses_title).to_have_text("There is no results")
 
-            courses_title = page.get_by_test_id("courses-list-empty-view-title-text")
-            expect(courses_title).to_have_text("There is no results")
-
-            courses_discription = page.get_by_test_id("courses-list-empty-view-description-text")
-            expect(courses_discription).to_have_text("Results from the load test pipeline will be displayed here")
+        courses_description = page.get_by_test_id("courses-list-empty-view-description-text")
+        expect(courses_description).to_have_text("Results from the load test pipeline will be displayed here")
