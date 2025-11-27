4f3bcb9cfe4681d115ef317bf2059ebdce173576
Egor <steam.egor585@yandex.ru>
2025-11-27 23:10:08 +0300

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent#1

--- pages/create_course_page.py
+++ pages/create_course_page.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Page, expect
 from pages.base_page import BasePage
 from components.views.empty_view_component import EmptyViewComponent
 from components.views.image_upload_widget_component import ImageUploadWidgetComponent
-
+from components.courses.create_course_exercise_form_component import CreateCourseExerciseFormComponent
 
 class CreateCoursePage(BasePage):
     def __init__(self, page: Page):
@@ -12,6 +12,7 @@ class CreateCoursePage(BasePage):
         self.preview_empty_view = EmptyViewComponent(page, "create-course-preview")
         self.exercises_empty_view = EmptyViewComponent(page, "create-course-exercises")
         self.image_upload_widget = ImageUploadWidgetComponent(page, "create-course-preview")
+        self.create_exercise_form = CreateCourseExerciseFormComponent(page)
         
         self.create_course_title = page.get_by_test_id('create-course-toolbar-title-text')
         self.create_course_button = page.get_by_test_id('create-course-toolbar-create-course-button')
@@ -92,9 +93,6 @@ class CreateCoursePage(BasePage):
         expect(self.exercises_title).to_be_visible()
         expect(self.exercises_title).to_have_text("Exercises")
 
-    def check_visible_create_exercise_button(self):
-        expect(self.create_exercise_button).to_be_visible()
-
     def click_create_exercise_button(self):
         self.create_exercise_button.click()
 
@@ -103,45 +101,3 @@ class CreateCoursePage(BasePage):
             title="There is no exercises",
             description='Click on "Create exercise" button to create new exercise',
         )
-
-    def click_delete_exercise_button(self, index: int):
-        delete_exercise_button = self.page.get_by_test_id(
-            f"create-course-exercise-{index}-box-toolbar-delete-exercise-button"
-        )
-        delete_exercise_button.click()
-
-    def check_visible_create_exercise_form(
-        self, index: int, title: str, description: str
-    ):
-        exercise_subtitle = self.page.get_by_test_id(
-            f"create-course-exercise-{index}-box-toolbar-subtitle-text"
-        )
-        exercise_title_input = self.page.get_by_test_id(
-            f"create-course-exercise-form-title-{index}-input"
-        )
-        exercise_description_input = self.page.get_by_test_id(
-            f"create-course-exercise-form-description-{index}-input"
-        )
-
-        expect(exercise_subtitle).to_be_visible()
-        expect(exercise_subtitle).to_have_text(f"#{index + 1} Exercise")
-
-        expect(exercise_title_input).to_be_visible()
-        expect(exercise_title_input).to_have_value(title)
-
-        expect(exercise_description_input).to_be_visible()
-        expect(exercise_description_input).to_have_value(description)
-
-    def fill_create_exercise_form(self, index: int, title: str, description: str):
-        exercise_title_input = self.page.get_by_test_id(
-            f"create-course-exercise-form-title-{index}-input"
-        )
-        exercise_description_input = self.page.get_by_test_id(
-            f"create-course-exercise-form-description-{index}-input"
-        )
-
-        exercise_title_input.fill(title)
-        expect(exercise_title_input).to_have_value(title)
-
-        exercise_description_input.fill(description)
-        expect(exercise_description_input).to_have_value(description)
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -13,10 +13,10 @@ def test_successful_registration(
     registration_page.visit(
         "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration"
     )
-    registration_page.fill_registration_form(
+    registration_page.registration_form.fill(
         email=random_user_for_registration["email"],
         username=random_user_for_registration["username"],
         password=random_user_for_registration["password"],
     )
     registration_page.click_registration_button()
-    dashboard_page.verify_title_on_dashboard()
+    dashboard_page.dashboard_title.check_visible()
